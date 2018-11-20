463112bfd1734570df6c174179839379b9915106
Noah Woods <noahwoods@gmail.com>
2018-11-20 11:04:09 -0600

    moved fetch calls to different modules

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -4,45 +4,17 @@ import AnimalList from './animal/AnimalList'
 import LocationList from './locations/LocationList'
 import EmployeeList from './employee/EmployeeList'
 import OwnerList from './owner/OwnerList'
+import AnimalManager from "../modules/AnimalManager"
+import EmployeeManager from "../modules/EmployeeManager"
+import OwnerManager from "../modules/OwnerManager"
+import LocationManager from "../modules/LocationManager"
+
+
+
 
 
 class ApplicationViews extends Component {
 
-  // hard coded data
-  // employeesFromAPI = [
-  //     { id: 1, name: "Jessica Younker" },
-  //     { id: 2, name: "Jordan Nelson" },
-  //     { id: 3, name: "Zoe LeBlanc" },
-  //     { id: 4, name: "Blaise Roberts" }
-  // ]
-
-  // locationsFromAPI = [
-  //     { id: 1, name: "Nashville North", address: "500 Doggo Lane" },
-  //     { id: 2, name: "Nashville South", address: "114 Meow Street" }
-  // ]
-
-  // animalsFromAPI = [
-  //     { id: 1, name: "Doodles" },
-  //     { id: 2, name: "Jack" },
-  //     { id: 3, name: "Angus" },
-  //     { id: 4, name: "Henley" },
-  //     { id: 5, name: "Derkins" },
-  //     { id: 6, name: "Checkers" }
-  // ]
-
-  // ownersFromAPI = [
-  //   { id: 1, name: "Peter Parker", phone: "774-337-6261"},
-  //   { id: 2, name: "Scott Lang", phone: "843-268-6261"},
-  //   { id: 3, name: "Peter Quill", phone: "467-827-5673"},
-  //   { id: 4, name: "Tony Stark", phone: "843-626-4766"}
-  // ]
-
-  // state = {
-  //     employees: this.employeesFromAPI,
-  //     locations: this.locationsFromAPI,
-  //     animals: this.animalsFromAPI,
-  //     owners: this.ownersFromAPI
-  // }
   state = {
     employees: [],
     locations: [],
@@ -52,20 +24,20 @@ class ApplicationViews extends Component {
 
   componentDidMount() {
     const newState = {}
+    AnimalManager.getAll().then(allAnimals => {
+      newState.animals = allAnimals
+    })
+    EmployeeManager.getAll().then(allEmployees => {
+    newState.employees = allEmployees
+    })
+    LocationManager.getAll().then(allLocations => {
+      newState.locations = allLocations
+    })
+    OwnerManager.getAll().then(allOwners => {
+      newState.owners = allOwners
+    })
+.then(() => this.setState(newState))
 
-    fetch("http://localhost:5002/animals")
-      .then(r => r.json())
-      .then(animals => newState.animals = animals)
-      .then(() => fetch("http://localhost:5002/employees")
-        .then(r => r.json()))
-      .then(employees => newState.employees = employees)
-      .then(() => fetch("http://localhost:5002/locations")
-        .then(r => r.json()))
-      .then(locations => newState.locations = locations)
-      .then(() => fetch("http://localhost:5002/owners")
-        .then(r => r.json()))
-      .then(owners => newState.owners = owners)
-      .then(() => this.setState(newState))
   }
 
   deleteAnimal = id => {
@@ -114,7 +86,8 @@ class ApplicationViews extends Component {
     return (
       <React.Fragment>
         <Route exact path="/" render={(props) => {
-          return <LocationList locations={this.state.locations} />
+          return <LocationList
+            locations={this.state.locations} />
         }} />
         <Route path="/animals" render={(props) => {
           return (
--- src/components/locations/LocationList.js
+++ src/components/locations/LocationList.js
@@ -1,36 +1,24 @@
 import React, { Component } from 'react'
 
-// export default class LocationList extends Component {
-  // render() {
-  //   return (
-  //     <section>
-  //       <h1>Locations</h1>
-  //       <section>Nashville North</section>
-  //         <div>500 Doggo Ln</div>
-  //       <section>Nashville South</section>
-  //         <div>100 Meow St</div>
-  //     </section>
-  //   );
-  // }
-  export default class LocationList extends Component {
-    render() {
-      return (
-        <section className="locations">
-          <h1>Locations:</h1>
-          {
-            this.props.locations.map(location =>
-              <div key={location.id}>
-              <div>
-                {location.name}
-                <br></br>
-                {location.address}
-                <br></br>
+export default class LocationList extends Component {
+  render() {
+    return (
+      <section className="locations">
+        <h1>Locations:</h1>
+        {
+          this.props.locations.map(location =>
+            <div key={location.id} >
+                <div>
+                  {location.name}
+                  <br></br>
+                  {location.address}
+                  <br></br>
+                  </div>
               </div>
-              </div>
-            )
-          }
+              )
+            }
         </section>
-      );
-    }
+          );
+        }
 
 }
